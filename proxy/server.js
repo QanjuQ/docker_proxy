a0c322cadf34d445fda610dc6f27b813851b35e8
Anjum Qureshi <anjumqureshi000@gmail.com>
2019-02-06 18:18:57 +0530

    Remove external port mappings

--- proxy/server.js
+++ proxy/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const port = 9000;
+const port = process.env.PORT;
 
-const webServices = ["web1", "web2", "web3"];
-
-const options = {
-  port: 9000,
-  method: 'GET',
-};
+const webServices = process.env.SERVICES.split(' ');
+let hosts = webServices;
 
 const cycle = (list) => {
   let cycledList = list.slice();
@@ -16,21 +12,28 @@ const cycle = (list) => {
   return cycledList;
 };
 
-let hosts = webServices;
+const options = (req) => {
+  let data = "";
+  let reqBody = req.on('data', (chunk) => data += chunk);
+  return {
+    path: `http://${hosts[0]}:${port}${req.url}`,
+    host: hosts[0],
+    port: port,
+    method: req.method,
+    body: reqBody
+  };
+}
 
 app.get('/', (req, res) => {
-  options.host = hosts[0];
-  options.path = `http://${hosts[0]}:${options.port}/`;
-  console.log(options.path);
-  const requestWeb = http.request(options, (webResponse) => {
+  const requestWeb = http.request(options(req), (webResponse) => {
     webResponse.setEncoding('utf8');
     let data = '';
     webResponse.on('data', (chunk) => data += chunk);
     webResponse.on('end', () => res.send('Hello World!'));
   });
   hosts = cycle(hosts);
+  requestWeb.write(options.body);
   requestWeb.end();
 });
 
 app.listen(port, () => console.log(`Welcome to proxiness ${port}!`));
-
--- web/server.js
+++ web/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 9000;
 
-app.get('/', (req, res) => { console.log(req.url, req.method, " hello"); res.send('Hello World!'); });
+app.get('/', (req, res) => { console.log(req.url, req.method); res.send('Hello World!'); });
 
 app.listen(port, () => console.log(`Magic happens here ${port}!`));
