6be12fd3d581e565f1d0b8f342cdc769fdb1cfd7
Anjum Qureshi <anjumqureshi000@gmail.com>
2019-02-08 10:19:52 +0530

    Add functionality to serve even with no service running

--- proxy/server.js
+++ proxy/server.js
@@ -2,37 +2,75 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const port = process.env.PORT;
-
-const webServices = process.env.SERVICES.split(' ');
-let hosts = webServices;
-
-const cycle = (list) => {
-  let cycledList = list.slice();
-  cycledList.push(cycledList.shift());
-  return cycledList;
+const now = () => new Date().getTime();
+const logger = function (req, res, next) {
+  console.log(req.url, req.method);
+  next();
 };
 
+class Service {
+  constructor (ip) {
+    this.ip = ip;
+    this._time = new Date().getTime();
+  }
+
+  isAlive(limit) {
+    return now() - this._time < limit;
+  }
+}
+
+class Services {
+  constructor (services = []) {
+    console.log(services);
+    this.services = services;
+  }
+
+  addService(service) {
+    this.services.push(service);
+  }
+
+  aliveServices(limit) {
+    const services = this.services.filter((service) => service.isAlive(limit));
+    return new Services(services);
+  }
+}
+
+let services = new Services();
+
 const options = (req) => {
+  services = services.aliveServices(2000).services;
+  let host = services[0].ip;
   let data = "";
   let reqBody = req.on('data', (chunk) => data += chunk);
   return {
-    path: `http://${hosts[0]}:${port}${req.url}`,
-    host: hosts[0],
+    path: req.url,
+    host: host,
     port: port,
     method: req.method,
     body: reqBody
   };
-}
+};
 
-app.get('/', (req, res) => {
-  const requestWeb = http.request(options(req), (webResponse) => {
+app.use(logger);
+
+app.post('/service-health', (req, res) => {
+  const service = new Service(req.connection.remoteAddress);
+  services.addService(service);
+  res.end("OK");
+});
+
+app.use((req, res) => {
+  let reqConfig = options(req);
+  if (reqConfig.host == undefined) {
+    res.end("Server not present");
+  }
+  const requestWeb = http.request(reqConfig, (webResponse) => {
     webResponse.setEncoding('utf8');
     let data = '';
     webResponse.on('data', (chunk) => data += chunk);
     webResponse.on('end', () => res.send('Hello World!'));
   });
-  hosts = cycle(hosts);
-  requestWeb.write(options.body);
+  requestWeb.write(reqConfig.body.toString());
   requestWeb.end();
 });
 
--- web/server.js
+++ web/server.js
@@ -1,7 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const http = require("http");
+const app = express();
 const port = 9000;
+const host = process.env.LOAD_BALANCER || "proxy";
+const sleepingTime = 10000000000;
 
-app.get('/', (req, res) => { console.log(req.url, req.method); res.send('Hello World!'); });
+const logger = function (req, res, next) {
+  console.log(req.url, req.method);
+  next();
+};
+
+const sleep = (time, interval) => {
+  clearInterval(interval);
+  while (time)
+    time--;
+};
+
+const options = {
+  path: '/service-health',
+  host: host,
+  port: 9000, //parametrize it.
+  method: 'POST'
+};
+
+
+const postHealth = () => setInterval(() => {
+  let req = http.request(options, (response) => {
+    if (response.statusCode == 200) {
+      console.log("Health post successful.");
+    }
+  });
+  req.write("health");
+  req.end();
+}, 2000);
+
+let startpush = postHealth();
+
+
+app.use(logger);
+app.get('/sleep', (req, res) => {
+  sleep(sleepingTime, startpush);
+  startpush = postHealth;
+  res.send("I will be busy sleeping for some time.");
+});
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/', (req, res) => res.send('Hello World!,post'));
 
 app.listen(port, () => console.log(`Magic happens here ${port}!`));
